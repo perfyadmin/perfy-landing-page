89bbab1e24eebefbd1080085d5ebd94c253170af
exceptionzofficial <exceptionzofficial@gmail.com>
2026-04-17 09:51:35 +0530

    updaed error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +10,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsconfigPaths(),
-    process.env.NODE_ENV === "production" && cloudflare(),
-  ].filter(Boolean),
+  ],
 });
